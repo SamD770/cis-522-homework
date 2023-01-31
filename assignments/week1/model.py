cb30eac3dd6377a13c1fef8141c8ef7ef20046eb
SamD770 <samdauncey.sd@gmail.com>
2023-01-30 19:09:39 -0500

    docstrings and dynamic number of features

--- assignments/week1/model.py
+++ assignments/week1/model.py
@@ -1,9 +1,6 @@
 import numpy as np
 
 
-FEATURES = 8
-
-
 def add_bias_feature(X):
     samples, features = X.shape
     dummy_feature = np.ones((samples, 1))
@@ -18,22 +15,54 @@ def separate_bias(w_b_concat):
 
 
 class LinearRegression:
+    """
+    A linear regression model that uses analytic results to fit the model.
+    """
 
     w: np.ndarray
     b: float
 
     def __init__(self):
-        self.w = np.random.uniform(low=-0.1, high=0.1, size=FEATURES)
+        self.w = None
+        self.b = None
+
+    def initialise_params(self, features: int) -> None:
+        self.w = np.random.uniform(low=-0.1, high=0.1, size=features)
         self.b = np.random.uniform(low=-1, high=1)
 
-    def fit(self, X, y):
+    def fit(self, X: np.ndarray, y: np.ndarray) -> None:
+        """
+        Fits the model parameters using the provided data.
+
+        Arguments:
+            X (np.ndarray): The input data.
+            y (np.ndarray): The input labels
+        """
+        if self.w is None:
+            samples, features = X.shape
+            self.initialise_params(features)
+
         X_concat = add_bias_feature(X)
 
         w_b_concat = np.linalg.inv(X_concat.T @ X_concat) @ X_concat.T @ y
 
         self.w, self.b = separate_bias(w_b_concat)
 
-    def predict(self, X):
+    def predict(self, X: np.ndarray) -> np.ndarray:
+        """
+        Predict the output for the given input.
+
+        Arguments:
+            X (np.ndarray): The input data.
+
+        Returns:
+            np.ndarray: The predicted output.
+
+        """
+        if self.w is None:
+            samples, features = X.shape
+            self.initialise_params(features)
+
         return X @ self.w + self.b
 
 
@@ -44,7 +73,10 @@ class GradientDescentLinearRegression(LinearRegression):
 
     def fit(self, X: np.ndarray, y: np.ndarray, lr: float = 1e-7, epochs: int = 1000):
 
-        n_samples, _ = X.shape
+        n_samples, features = X.shape
+
+        if self.w is None:
+            self.initialise_params(features)
 
         for epoch in range(epochs):
 
