3f62798be567ddc1f9ad6bda1aca968061c4624d
SamD770 <samdauncey.sd@gmail.com>
2023-03-21 17:45:19 -0400

    used model from colab notebook

--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -5,26 +5,46 @@ from torch.nn import functional as F
 
 class Model(torch.nn.Module):
     """A simple convnet architecture."""
-
     def __init__(self, num_channels: int, num_classes: int) -> None:
         """Initialise the model."""
         super(Model, self).__init__()
-        self.conv1 = nn.Conv2d(num_channels, 32, 3, padding=1)
-        self.conv2 = nn.Conv2d(32, 32, 3, padding=1)
-        self.linear1 = nn.Linear(32 * 8 * 8, 128)
-        self.linear2 = nn.Linear(128, num_classes)
+        self.conv1 = nn.Conv2d(1, 32, 3, 1)
+        self.conv2 = nn.Conv2d(32, 64, 3, 1)
+        self.fc1 = nn.Linear(9216, 128)
+        self.fc2 = nn.Linear(128, 10)
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
+    def forward(self, x):
         """Outputs the logits for a given input."""
         x = self.conv1(x)
         x = F.relu(x)
-        x = F.max_pool2d(x, 2)
         x = self.conv2(x)
         x = F.relu(x)
         x = F.max_pool2d(x, 2)
         x = torch.flatten(x, 1)
-        x = self.linear1(x)
+        x = self.fc1(x)
         x = F.relu(x)
-        x = self.linear2(x)
-
+        x = self.fc2(x)
         return x
+
+# def __init__(self, num_channels: int, num_classes: int) -> None:
+#     """Initialise the model."""
+#     super(Model, self).__init__()
+#     self.conv1 = nn.Conv2d(num_channels, 32, 3, padding=1)
+#     self.conv2 = nn.Conv2d(32, 32, 3, padding=1)
+#     self.linear1 = nn.Linear(32 * 8 * 8, 128)
+#     self.linear2 = nn.Linear(128, num_classes)
+#
+# def forward(self, x: torch.Tensor) -> torch.Tensor:
+#     """Outputs the logits for a given input."""
+#     x = self.conv1(x)
+#     x = F.relu(x)
+#     x = F.max_pool2d(x, 2)
+#     x = self.conv2(x)
+#     x = F.relu(x)
+#     x = F.max_pool2d(x, 2)
+#     x = torch.flatten(x, 1)
+#     x = self.linear1(x)
+#     x = F.relu(x)
+#     x = self.linear2(x)
+#
+#     return x
