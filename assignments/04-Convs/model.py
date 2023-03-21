db1ecc3be136b4e5eb5b3836a61595044d3edb93
SamD770 <samdauncey.sd@gmail.com>
2023-03-21 17:53:49 -0400

    size adjustments

--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -9,10 +9,10 @@ class Model(torch.nn.Module):
     def __init__(self, num_channels: int, num_classes: int) -> None:
         """Initialise the model."""
         super(Model, self).__init__()
-        self.conv1 = nn.Conv2d(1, 32, 3, 1)
+        self.conv1 = nn.Conv2d(num_channels, 32, 3, 1)
         self.conv2 = nn.Conv2d(32, 64, 3, 1)
-        self.fc1 = nn.Linear(9216, 128)
-        self.fc2 = nn.Linear(128, 10)
+        self.fc1 = nn.Linear(64*14*14, 128)
+        self.fc2 = nn.Linear(128, num_classes)
 
     def forward(self, x):
         """Outputs the logits for a given input."""
